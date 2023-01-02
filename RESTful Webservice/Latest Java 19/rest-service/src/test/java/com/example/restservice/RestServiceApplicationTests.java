b09b37972426ac158f3b08a2cd2230b1ef21ca01
AchuthanKumar <46110128+AchuthanKumar@users.noreply.github.com>
2023-01-02 18:01:29 +0530

    Initial Upload

---
+++ RESTful Webservice/Latest Java 19/rest-service/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+//	@BeforeClass
+//	public static void init(){
+//		RestAssured.baseURI = "http://localhost";
+//		RestAssured.port = 8080;
+//	}
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
